e6b84805b88224cbd781966177fcc9449ca985e7
Krystian Sz <sztadii@gmail.com>
2023-03-27 15:57:02 +0700

    Remove cypress tests
    API is down too often to rely on it, and we have proper integration tests

--- cypress/plugins/index.ts
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line no-unused-vars, import/no-anonymous-default-export
-export default function () {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
